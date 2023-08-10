63b224366142db23d152af44476d7bae60c13914
AJ <a.juramirza@gmail.com>
2023-08-10 16:52:47 +0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Akbarjon Juramirzaev on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
