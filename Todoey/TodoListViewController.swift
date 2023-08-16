dd269c2a60f7c2b3ebc3c984488e479d6889cb5f
AJ <a.juramirza@gmail.com>
2023-08-16 12:23:26 +0500

    1. Add /TableView Delegate/ method -> to put checkmark. 2. Create /Add (+)/  button -> to add new item into list.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Learn iOS", "Do Work out", "Do research on news in iOS"]
+    var itemArray = ["Learn iOS", "Do Work out", "Do research on news in iOS"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Create Add (+) Button
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
     }
     
     //MARK: - TableView Datasource Controller
@@ -33,6 +36,40 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
-
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let isRowSelected = tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        tableView.cellForRow(at: indexPath)?.accessoryType = isRowSelected ? .none : .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Items
+    @objc func addButtonPressed() {
+        
+        var textField = UITextField()
+        
+        // Create Alert pop-up
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        // Create action
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        // Add TextField to Alert
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        // Add action to Alert
+        alert.addAction(action)
+        
+        // Present Alert pop-up
+        present(alert, animated: true, completion: nil)
+    }
 }
 
